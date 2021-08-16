9878e275aebbe24c46081dfab4afd17384580532
Faith Chia <faithchiasuchi@gmail.com>
2021-08-16 18:03:59 +0800

    Updated travis.yml for CI

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh deploy@sus.picio.us "bash -c 'exit'" && echo "ssh deploy@sus.picio.us"
-ssh deploy@sus.picio.us "bash -c 'rm -rf /var/sus.picio.us/html/*'" && echo "ssh deploy@sus.picio.us deleted /var/..."
+echo "Starting deploy"
+ssh deploy@sus.picio.us "bash -c 'rm -rf /var/sus.picio.us/html/*' && echo 'success deleted' " && echo "ssh deploy@sus.picio.us deleted /var/..."
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build deploy@sus.picio.us:/var/sus.picio.us/html/ && echo "rsync"
